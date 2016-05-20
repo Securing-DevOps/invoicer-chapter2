a096718c4bceaf792dad7a4a4b070ec1a0155efc
Julien Vehent <julien@linuxwall.info>
2016-05-20 17:43:52 -0400

    init

---
+++ main.go
@@ -0,0 +1,177 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Contributor: Julien Vehent jvehent@mozilla.com [:ulfr]
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+type invoicer struct {
+	db *gorm.DB
+}
+
+func main() {
+	var (
+		iv  invoicer
+		err error
+	)
+	var db *gorm.DB
+	if os.Getenv("INVOICER_USE_POSTGRES") != "" {
+		db, err = gorm.Open("postgres", fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=%s",
+			os.Getenv("INVOICER_POSTGRES_USER"),
+			os.Getenv("INVOICER_POSTGRES_PASSWORD"),
+			os.Getenv("INVOICER_POSTGRES_HOST"),
+			os.Getenv("INVOICER_POSTGRES_DB"),
+			os.Getenv("INVOICER_POSTGRES_SSLMODE"),
+		))
+	} else {
+		db, err = gorm.Open("sqlite3", "/tmp/invoicer.db")
+	}
+	if err != nil {
+		panic("failed to connect database")
+	}
+	iv.db = db
+	iv.db.AutoMigrate(&Invoice{}, &Charge{})
+	iv.db.LogMode(true)
+
+	// register routes
+	r := mux.NewRouter()
+	r.HandleFunc("/__heartbeat__", getHeartbeat).Methods("GET")
+	r.HandleFunc("/invoice/{id:[0-9]+}", iv.getInvoice).Methods("GET")
+	r.HandleFunc("/invoice", iv.postInvoice).Methods("POST")
+	r.HandleFunc("/invoice/{id:[0-9]+}", iv.putInvoice).Methods("PUT")
+	r.HandleFunc("/invoice/{id:[0-9]+}", iv.deleteInvoice).Methods("DELETE")
+
+	// all set, start the http handler
+	err = http.ListenAndServe("localhost:8080", r)
+	if err != nil {
+		panic(err)
+	}
+}
+
+type Invoice struct {
+	gorm.Model
+	IsPaid      bool      `json:"is_paid"`
+	Amount      int       `json:"amount"`
+	PaymentDate time.Time `json:"payment_date"`
+	DueDate     time.Time `json:"due_date"`
+	Charges     []Charge  `json:"charges"`
+}
+
+type Charge struct {
+	gorm.Model
+	InvoiceID   int     `gorm:"index"  json:"invoice_id"`
+	Type        string  `json:"type"`
+	Amount      float64 `json:"amount"`
+	Description string  `json:"description"`
+}
+
+func (iv *invoicer) getInvoice(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	log.Println("getting invoice id", vars["id"])
+	var i1 Invoice
+	id, _ := strconv.Atoi(vars["id"])
+	iv.db.First(&i1, id)
+	fmt.Printf("%+v\n", i1)
+	if i1.ID == 0 {
+		httpError(w, http.StatusNotFound, "No invoice id %s", vars["id"])
+		return
+	}
+	iv.db.Where("invoice_id = ?", i1.ID).Find(&i1.Charges)
+	jsonInvoice, err := json.Marshal(i1)
+	if err != nil {
+		httpError(w, http.StatusInternalServerError, "failed to retrieve invoice id %d: %s", vars["id"], err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonInvoice)
+}
+
+func (iv *invoicer) postInvoice(w http.ResponseWriter, r *http.Request) {
+	log.Println("posting new invoice")
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		httpError(w, http.StatusBadRequest, "failed to read request body: %s", err)
+		return
+	}
+	var i1 Invoice
+	err = json.Unmarshal(body, &i1)
+	if err != nil {
+		httpError(w, http.StatusBadRequest, "failed to parse request body: %s", err)
+		return
+	}
+	// make sure the IDs are null before inserting
+	i1.ID = 0
+	for i := 0; i < len(i1.Charges); i++ {
+		i1.Charges[i].ID = 0
+		i1.Charges[i].InvoiceID = 0
+	}
+	iv.db.Create(&i1)
+	iv.db.Last(&i1)
+	log.Printf("%+v\n", i1)
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(fmt.Sprintf("created invoice %d", i1.ID)))
+}
+
+func (iv *invoicer) putInvoice(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	log.Println("updating invoice", vars["id"])
+	var i1 Invoice
+	iv.db.First(&i1, vars["id"])
+	if i1.ID == 0 {
+		httpError(w, http.StatusNotFound, "No invoice id %s", vars["id"])
+		return
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		httpError(w, http.StatusBadRequest, "failed to read request body: %s", err)
+		return
+	}
+	err = json.Unmarshal(body, &i1)
+	if err != nil {
+		httpError(w, http.StatusBadRequest, "failed to parse request body: %s", err)
+		return
+	}
+	iv.db.Save(&i1)
+	iv.db.First(&i1, vars["id"])
+	log.Printf("%+v\n", i1)
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte(fmt.Sprintf("updated invoice %d", i1.ID)))
+}
+
+func (iv *invoicer) deleteInvoice(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	log.Println("deleting invoice", vars["id"])
+	var i1 Invoice
+	id, _ := strconv.Atoi(vars["id"])
+	iv.db.Where("invoice_id = ?", id).Delete(Charge{})
+	i1.ID = uint(id)
+	iv.db.Delete(&i1)
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte(fmt.Sprintf("deleted invoice %d", i1.ID)))
+}
+
+func getHeartbeat(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("I am alive"))
+}
+
+func httpError(w http.ResponseWriter, errorCode int, errorMessage string, args ...interface{}) {
+	log.Printf("%d: %s", errorCode, fmt.Sprintf(errorMessage, args...))
+	http.Error(w, fmt.Sprintf(errorMessage, args...), errorCode)
+	return
+}
