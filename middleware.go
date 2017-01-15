abe930fccc1aa26fd0ab9844fc295c669b276447
Julien Vehent <julien@linuxwall.info>
2017-01-15 09:22:53 -0500

    Add X-XSS-Protection header

--- middleware.go
+++ middleware.go
@@ -39,6 +39,7 @@ func setResponseHeaders() Middleware {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Add("Content-Security-Policy", "default-src 'self'; child-src 'self;")
+			w.Header().Add("X-XSS-Protection", "1; mode=block")
 			w.Header().Add("X-Frame-Options", "SAMEORIGIN")
 			w.Header().Add("X-Content-Type-Options", "nosniff")
 			w.Header().Add("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
