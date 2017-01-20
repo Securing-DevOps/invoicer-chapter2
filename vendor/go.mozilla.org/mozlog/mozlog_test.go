be45f1b80c1e46602716d263c4452aa4b4d81d5f
Julien Vehent <julien@linuxwall.info>
2017-01-20 14:32:32 -0500

    vendoring update

--- vendor/go.mozilla.org/mozlog/mozlog_test.go
+++ vendor/go.mozilla.org/mozlog/mozlog_test.go
@@ -23,19 +23,3 @@ func TestMozLogger(t *testing.T) {
 
 	assert.Equal(t, "test message", logEntry.Fields["msg"])
 }
-
-func TestJSONLogger(t *testing.T) {
-	log.SetFlags(0)
-
-	in := new(bytes.Buffer)
-	Logger.Output = in
-
-	log.Println(`{"a": 1234, "b": true, "c": [1, 2]}`)
-
-	var logEntry AppLog
-	err := json.Unmarshal(in.Bytes(), &logEntry)
-	assert.NoError(t, err)
-	lefields, err := json.Marshal(logEntry.Fields)
-	assert.NoError(t, err)
-	assert.JSONEq(t, `{"b": true, "c": [1, 2], "a": 1234}`, string(lefields))
-}
