de463d0a78f794b3da8faa95e0fe3c6907af206f
chinds <chinds@localhost.localdomain>
2019-01-22 19:43:15 -0500

    updated script due to select correct docker stack

--- create_ebs_env.sh
+++ create_ebs_env.sh
@@ -77,7 +77,7 @@ echo "ElasticBeanTalk application created"
 
 # Get the name of the latest Docker solution stack
 dockerstack="$(aws elasticbeanstalk list-available-solution-stacks | \
-    jq -r '.SolutionStacks[]' | grep -P '.+Amazon Linux.+Docker.+' | head -1)"
+    jq -r '.SolutionStacks[]' | grep -P '64bit Amazon Linux 2018.03 v2.12.6 running Docker 18.06.1-ce' | head -1)"
 
 # Create the EB API environment
 sed "s/POSTGRESPASSREPLACEME/$dbpass/" ebs-options.json > tmp/$identifier/ebs-options.json || fail
