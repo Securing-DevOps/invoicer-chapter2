0891d23fcfceeff834c2765ff6434af5c8c81626
tinchocafferata <51465193+tinchocafferata@users.noreply.github.com>
2020-04-05 18:47:30 -0300

    Update main.go

--- main.go
+++ main.go
@@ -236,7 +236,7 @@ func getHeartbeat(w http.ResponseWriter, r *http.Request) {
 // handleVersion returns the current version of the API
 func getVersion(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf(`{
-"source": "https://github.com/Securing-DevOps/invoicer",
+"sourceee": "https://github.com/Securing-DevOps/invoicer",
 "version": "%s",
 "commit": "%s",
 "build": "https://circleci.com/gh/Securing-DevOps/invoicer/"
