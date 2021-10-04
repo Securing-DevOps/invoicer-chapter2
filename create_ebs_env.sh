5b986edf255ec5fd61d7387f4bcf65be79354580
deonGuo <deon.guo@gmail.com>
2021-10-04 08:45:07 +0000

    updated dbinstclass="db.t2.micro" to "db.t2.small"
    because my aws education starter account does not allow t2.micro

--- create_ebs_env.sh
+++ create_ebs_env.sh
@@ -32,7 +32,8 @@ dbsg=$(jq -r '.GroupId' tmp/$identifier/dbsg.json)
 echo "DB security group is $dbsg"
 
 # Create the database
-dbinstclass="db.t2.micro"
+#dbinstclass="db.t2.micro"
+dbinstclass="db.t2.small"
 dbstorage=5
 dbpass=$(dd if=/dev/urandom bs=128 count=1 2>/dev/null| tr -dc _A-Z-a-z-0-9)
 aws rds create-db-instance \
