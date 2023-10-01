988635a8ac2d7d2879c15198ff3bbad883181495
ioseroi <vladnikitin2000@gmail.com>
2023-10-01 14:49:30 +0300

    h1 edit

--- main.go
+++ main.go
@@ -211,7 +211,7 @@ func (iv *invoicer) getIndex(w http.ResponseWriter, r *http.Request) {
         <link href="statics/style.css" rel="stylesheet">
     </head>
     <body>
-	<h1>Invoicer Web</h1>
+	<h1>Invoicer Web Service</h1>
         <p class="desc-invoice"></p>
         <div class="invoice-details">
         </div>
